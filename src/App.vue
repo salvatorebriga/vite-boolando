eb4573643fea83d74e3d88f35e1d62ea6794dd1c
salvatorebriga <salvo.z2302@gmail.com>
2024-05-30 16:27:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  import AppHeader from "./components/AppHeader.vue";
+  export default {
+    name: "MyApp",
+    data() {
+      return {};
+    },
+    components: {
+      AppHeader,
+    },
+  };
+</script>
+
+<template>
+  <AppHeader />
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<script>
+  export default {
+    name: "AppHeader",
+  };
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
