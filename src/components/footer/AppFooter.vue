54d180184c7663be60d972c9f9472bf412d9a05e
salvatorebriga <salvo.z2302@gmail.com>
2024-05-31 17:01:54 +0200

    added footer basic structure

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -1,7 +1,37 @@
 <script>
-  export default {};
+  import FootLeft from "./FootLeft.vue";
+  import FootRight from "./FootRight.vue";
+  export default {
+    components: {
+      FootLeft,
+      FootRight,
+    },
+  };
 </script>
 
-<template></template>
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <FootLeft />
+        <FootRight />
+      </div>
+    </div>
+  </footer>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+  footer {
+    background-color: #1a1a1a;
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding-top: 80px;
+      padding-bottom: 80px;
+      .row {
+        display: flex;
+        flex-wrap: wrap;
+      }
+    }
+  }
+</style>
---
+++ src/components/footer/FootLeft.vue
@@ -0,0 +1,39 @@
+<script>
+  export default {};
+</script>
+
+<template>
+  <div class="col">
+    <h2>Boolando S.R.L</h2>
+    <ul>
+      <li><a href="">Informazioni legali</a></li>
+      <li><a href="">Informativa sulla privacy</a></li>
+      <li><a href="">Diritto di recesso</a></li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .col {
+    width: 50%;
+    color: white;
+    h2 {
+      margin-bottom: 50px;
+    }
+    ul {
+      display: flex;
+      gap: 10px;
+      li {
+        list-style-type: none;
+        a {
+          color: white;
+          text-decoration: none;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+  }
+</style>
