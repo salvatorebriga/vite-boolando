a6f1fe7fc85def992595d2f7829399bd8ca33a84
salvatorebriga <salvo.z2302@gmail.com>
2024-05-31 17:08:55 +0200

    footer done

--- src/components/footer/FootRight.vue
+++ src/components/footer/FootRight.vue
@@ -2,6 +2,37 @@
   export default {};
 </script>
 
-<template></template>
+<template>
+  <div class="col">
+    <h3>Trovaci anche su</h3>
+    <ul>
+      <li><i class="fab fa-twitter-square"></i></li>
+      <li><i class="fab fa-facebook-square"></i></li>
+      <li><i class="fab fa-instagram-square"></i></li>
+      <li><i class="fab fa-pinterest-square"></i></li>
+      <li><i class="fab fa-youtube"></i></li>
+    </ul>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .col {
+    width: 50%;
+    color: white;
+    text-align: right;
+    h3 {
+      margin-bottom: 50px;
+    }
+    ul {
+      display: flex;
+      justify-content: right;
+      gap: 10px;
+      li {
+        list-style-type: none;
+        i {
+          font-size: 25px;
+        }
+      }
+    }
+  }
+</style>
