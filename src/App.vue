54de5cdb4b3ba7daf8f56ab40a3e6e6f76349c52
salvatorebriga <salvo.z2302@gmail.com>
2024-05-31 16:00:55 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
   import AppHeader from "./components/header/AppHeader.vue";
   import AppContent from "./components/content/AppContent.vue";
+  import AppFooter from "./components/footer/AppFooter.vue";
   export default {
     name: "MyApp",
     data() {
@@ -9,6 +10,7 @@
     components: {
       AppHeader,
       AppContent,
+      AppFooter,
     },
   };
 </script>
@@ -16,4 +18,5 @@
 <template>
   <AppHeader />
   <AppContent />
+  <AppFooter />
 </template>
